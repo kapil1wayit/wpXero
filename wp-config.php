dc154ee61c96ae9a610d1f10c7d51f5832d08586
gurbinder <gsgurbinder12@gmail.com>
2018-04-03 23:37:16 +0530

    push tpa work

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'wpdemo');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'admin786');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
